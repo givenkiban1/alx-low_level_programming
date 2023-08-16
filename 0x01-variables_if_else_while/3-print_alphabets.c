1b0705653a20afac629fc49705fbfa758b339ed9
Given <gkibnzait@gmail.com>
2023-08-15 20:02:17 -0700

    fixing betty bugs

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- * author - Given Kibanza
  * main - Printing the alphabets in both upper and lower case.
  * Return: 0
 */
@@ -10,13 +9,10 @@ int main(void)
 {
 	char lower;
 	char upper;
-
 	for (lower = 'a' ; lower <= 'z' ; lower++)
 		putchar(lower);
-	
 	for (upper = 'A' ; upper <= 'Z' ; upper++)
 		putchar(upper);
-
 	putchar(10);
 	return (0);
 }
